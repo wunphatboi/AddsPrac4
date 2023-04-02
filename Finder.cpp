e5f344157312e3f490e9ddb047a4fab91187f902
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2023-04-02 23:39:46 +0930

    code

--- Finder.cpp
+++ Finder.cpp
@@ -1,19 +1,18 @@
 #include "Finder.h"
 using namespace std;
 std::vector<int> Finder::findSubstrings(std::string s1, std::string s2){
-    std::vector<int> result(s2.size(), -1); 
-    std::unordered_map<std::string, int> index_map;
-    for (int i = 0; i < s1.size(); ++i) {
-        for (int j = 0; j < s2.size(); ++j) {
-            std::string prefix = s2.substr(0, j + 1);
-            if (index_map.find(prefix) == index_map.end() && s1.substr(i, j + 1) == prefix) {
-                index_map[prefix] = i;
-                result[j] = i;
-            }
+    std::vector<int> result;
+    std::unordered_map<std::string, int> prefixIndices;
+    for (int i = 0; i < s2.length(); i++) {
+        std::string prefix = s2.substr(0, i+1);
+        if (prefixIndices.find(prefix) == prefixIndices.end()) {
+            int index = s1.find(prefix);
+            prefixIndices[prefix] = (index == std::string::npos) ? -1 : index;
         }
-        if (index_map.size() == s2.size()) {
-            return result;
-        }
+    }
+    for (int i = 0; i < s2.length(); i++) {
+        std::string prefix = s2.substr(0, i+1);
+        result.push_back(prefixIndices[prefix]);
     }
     return result;
 }
