d42c107e853b4aa44ea5b55942ae7974c1515f86
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2023-04-03 10:22:53 +0930

    code

--- Finder.cpp
+++ Finder.cpp
@@ -1,42 +1,19 @@
 #include "Finder.h"
 using namespace std;
-std::vector<int> Finder::findSubstrings(std::string s1, std::string s2){
-     vector<int> result;
-    unordered_map<string, int> prefixMap;
-    int n = s1.size(), m = s2.size();
+vector<int> Finder::findSubstrings(string s1, string s2) {
+    vector<int> result(s2.size(), -1);
+    size_t last_found = string::npos;
 
-    // create prefixMap with all prefixes of s2
-    for (int i = 0; i < m; i++) {
-        string prefix = s2.substr(0, i+1);
-        if (prefixMap.find(prefix) == prefixMap.end()) {
-            prefixMap[prefix] = -1;
-        }
-    }
+    for (size_t i = 0; i < s2.size(); i++) {
+        // Only search for prefix if the previous prefix was found
+        size_t start_index = last_found == string::npos ? 0 : last_found + 1;
+        size_t found = s1.find(s2.substr(0, i + 1), start_index);
 
-    // search for prefixes in s1 and update prefixMap with their index
-    for (int i = 0; i < n; i++) {
-        string prefix = "";
-        for (int j = i; j < n; j++) {
-            prefix += s1[j];
-            if (prefixMap.find(prefix) != prefixMap.end()) {
-                if (prefixMap[prefix] == -1) {
-                    prefixMap[prefix] = i;
-                }
-            }
-        }
-    }
-
-    // add results to vector
-    for (int i = 0; i < m; i++) {
-        string prefix = s2.substr(0, i+1);
-        if (prefixMap[prefix] == -1) {
-            result.push_back(-1);
-        } else {
-            result.push_back(prefixMap[prefix]);
+        if (found != string::npos) {
+            result[i] = found;
+            last_found = found;
         }
     }
 
     return result;
 };
-
-
