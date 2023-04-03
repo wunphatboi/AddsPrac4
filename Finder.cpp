86287ff35edb83f24b63bbe61d7e8e453f378a3f
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2023-04-03 10:32:54 +0930

    code

--- Finder.cpp
+++ Finder.cpp
@@ -1,6 +1,6 @@
 #include "Finder.h"
 using namespace std;
-vector<int> Finder::findSubstrings(string s1, string s2) {
+/*vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result(s2.size(), -1);
     size_t index = 0; // index to start searching from in s1
 
@@ -14,5 +14,18 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
         }
     }
 
+    return result;
+};*/
+vector<int> Finder::findSubstrings(string s1, string s2) {
+    vector<int> result(s2.size(), -1);
+    size_t index = 0;
+
+    for (size_t i = 1; i <= s2.size(); i++) {
+        size_t found = s1.find(s2.substr(0, i), index);
+        if (found != string::npos) {
+            result[i-1] = found;
+            index = found;
+        }
+    }
     return result;
 };
